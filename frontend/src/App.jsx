c4d201be5247ef4a6b2d541f6eca6df41bbf6296
RohanMac <roku@bu.edu>
2024-03-29 21:17:24 -0400

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+
+// layout and home
+import Layout from "./Layout";
+
+// main app
 import "./App.css";
 
-function App() {
-    const [count, setCount] = useState(0);
-
+// creates routings of app --> used for sending pages when requested
+const App = () => {
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>authentech</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-        </>
+        <div>
+            <SnackbarProvider>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="*" element={<Error />} />
+                    </Route>
+                </Routes>
+            </SnackbarProvider>
+        </div>
     );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Header;
